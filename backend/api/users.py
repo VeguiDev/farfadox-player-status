8f6d990e204d5c2b5364150855fd49c9754898ad
VeguiDev <veguigameplays@gmail.com>
2023-06-17 01:02:04 -0300

    ⭐ Api call to get current player status

--- backend/api/users.py
+++ backend/api/users.py
@@ -11,3 +11,13 @@ async def getCurrentUser(access_token: str):
     headers.add("Content-Type", "application/json")
 
     return urllib3.request("GET", url=apiEndpoint + "/me", headers=headers)
+
+
+async def getPlayerStatus(access_token: str):
+    headers = urllib3.HTTPHeaderDict()
+
+    headers.add("accept", "application/json")
+    headers.add("Authorization", "Bearer " + access_token)
+    headers.add("Content-Type", "application/json")
+
+    return urllib3.request("GET", url=apiEndpoint + "/me/player", headers=headers)
