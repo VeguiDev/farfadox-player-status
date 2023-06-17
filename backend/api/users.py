a64d23b7097c7bf553454910d1d060cd8a65e8fc
VeguiDev <veguigameplays@gmail.com>
2023-06-17 00:13:30 -0300

    ⭐ users api

---
+++ backend/api/users.py
@@ -0,0 +1,13 @@
+import urllib3
+
+apiEndpoint = "https://api.spotify.com/v1"
+
+
+async def getCurrentUser(access_token: str):
+    headers = urllib3.HTTPHeaderDict()
+
+    headers.add("accept", "application/json")
+    headers.add("Authorization", "Bearer " + access_token)
+    headers.add("Content-Type", "application/json")
+
+    return urllib3.request("GET", url=apiEndpoint + "/me", headers=headers)
