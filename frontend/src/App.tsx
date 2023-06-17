697a342dea42f5df3f88893d793976199ba64bd6
VeguiDev <veguigameplays@gmail.com>
2023-06-17 02:58:14 -0300

    getting auth data on login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { PlayingNow } from './components/PlayingNow'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { PlayingNow } from "./components/PlayingNow";
+import AuthService from "./api/auth.service";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [logged, setLogged] = useState(false);
+
+  useEffect(() => {
+    AuthService.getCurrentUser().then((user) => {
+      setLogged(!!user);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading || !logged) {
+    return <></>;
+  }
+
   return (
     <>
       <PlayingNow></PlayingNow>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
