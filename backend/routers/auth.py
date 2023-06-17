3e2c4f4f73479c5c675e7682a7995d63ad1986d9
VeguiDev <veguigameplays@gmail.com>
2023-06-16 23:38:58 -0300

    routers

---
+++ backend/routers/auth.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter
+from fastapi.responses import RedirectResponse
+from ..api.oauth import requestAccessToken
+
+from ..config import client_id
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["authentication"]
+)
+
+
+@router.get("/")
+def getAuthInfo():
+    return {
+        "user": None
+    }
+
+
+@router.get("/oauth")
+def redirectToAuthorization():
+    scope = 'user-read-playback-state user-read-email user-read-currently-playing'
+
+    return RedirectResponse("https://accounts.spotify.com/authorize?response_type=code&client_id="+client_id+"&scope="+scope+"&redirect_uri=http://localhost:8000/auth/response&state=state")
+
+
+@router.get("/response")
+async def getAccessToken(code: str | None, state: str, error: str | None = None):
+    if (error != None):
+        return {
+            "error": error
+        }
+
+    resp = await requestAccessToken(code)
+
+    print(resp.status)
+
+    return resp.json()
