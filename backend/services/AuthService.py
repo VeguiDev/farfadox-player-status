12c2775d4f5ef93603f6b30da3441855e93040eb
VeguiDev <veguigameplays@gmail.com>
2023-06-17 01:15:02 -0300

    Auth Service

--- backend/services/AuthService.py
+++ backend/services/AuthService.py
@@ -10,12 +10,14 @@ class AuthService:
         self.authData.load()
 
     def tokenExpired(self):
+        self.authData.load()
         if self.authData.access_token != None and self.authData.expires_at != None:
             return time.time() > self.authData.expires_at
 
         return False
 
     async def tryRefreshAuthData(self):
+        self.authData.load()
         resp = await oauth.requestAccessTokenByRefreshToken(self.authData.refresh_token)
 
         if resp.status == 200:
@@ -31,6 +33,7 @@ class AuthService:
         return False
 
     async def validateAuthData(self):
+        self.authData.load()
         if not self.authData.refresh_token == None:
             if self.tokenExpired():
                 if not self.tryRefreshAuthData():
@@ -45,11 +48,13 @@ class AuthService:
             return False
 
     def isLoggedIn(self):
+        self.authData.load()
         auth = self.authData
 
         return auth.access_token != None and auth.refresh_token != None
 
     async def login(self, code: str):
+        self.authData.load()
         resp = await oauth.requestAccessToken(code)
 
         if resp.status == 200:
@@ -62,6 +67,7 @@ class AuthService:
         return False
 
     async def logout(self):
+        self.authData.load()
         if self.isLoggedIn():
             self.auth.refresh(null)
             return True
@@ -69,6 +75,7 @@ class AuthService:
         return False
 
     async def getValidAccessToken(self):
+        self.authData.load()
         if not self.tokenExpired():
             return self.authData.access_token
 
