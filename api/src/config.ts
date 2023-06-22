a87d2bea43c9bc25ea23c670e8a34d43f3744ea5
VeguiDev <veguigameplays@gmail.com>
2023-06-21 21:19:30 -0300

    new scope

--- api/src/config.ts
+++ api/src/config.ts
@@ -1,4 +1,4 @@
 export const client_id = "e289e3e42ff34a8b9475adb13aaba739";
 export const client_secret = "76f18a466a76458098ccba0cee5c9a71";
 export const token_scope =
-  "user-read-private user-read-email user-read-playback-state user-read-currently-playing";
+  "user-read-private user-read-email user-read-playback-state user-read-currently-playing user-top-read";
